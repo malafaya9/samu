18679fde4cf9ce60aad630d8f82d58e39e6abe27
Vinicius Albuquerque Malafaya <malafaya9@gmail.com>
2017-07-27 09:37:32 -0300

    Move Providers to App Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import {UF} from './types/uf';
-import {UFService} from './services/uf.service'
+import {UFService} from './services/uf.service';
 
 import {Dados} from './types/samu';
-import {SamuService} from './services/samu.service'
+import {SamuService} from './services/samu.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UFService, SamuService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
     meuEstado = 11;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { SamuService } from './services/samu.service';
+import { UFService } from './services/uf.service';
+
 import { AppComponent } from './app.component';
 import { TituloComponent } from './titulo/titulo.component';
 import { DetalhesComponent } from './detalhes/detalhes.component';
@@ -18,7 +21,7 @@ import { AtendimentosComponent } from './atendimentos/atendimentos.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [SamuService,UFService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
